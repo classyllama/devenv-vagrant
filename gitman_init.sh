466da747646e463ff28a0ecbc20f0f281e3a722f
Matt Johnson <matt.johnson@classyllama.com>
2020-02-01 20:24:21 -0600

    renaming files default / config variable overrides

--- gitman_init.sh
+++ gitman_init.sh
@@ -27,5 +27,11 @@ SOURCE_DIR_FROM_PERSIST_DIR="${GITMAN_LOCATION}/${EXPERIMENT_NAME}"
 [[ -f persistent/.gitignore ]] || cp .gitignore.sample persistent/.gitignore
 [[ -f persistent/Vagrantfile.config.rb ]] || cp Vagrantfile.config.rb.sample persistent/Vagrantfile.config.rb
 [[ -f persistent/Vagrantfile.local.rb ]] || cp Vagrantfile.local.rb.sample persistent/Vagrantfile.local.rb
+[[ -f persistent/devenv_vars.config.yml ]] || cp provisioning/devenv_vars.config.yml.sample persistent/devenv_vars.config.yml
+
+# Create symlinks in source to persistent files
+[[ -L provisioning/devenv_vars.config.yml ]] || ln -s ../persistent/devenv_vars.config.yml provisioning/devenv_vars.config.yml
+
+# app_vars.config.yml
 
 # TODO Add README to persistent directory with instructions on initializing with gitman or manually
